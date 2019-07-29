036324b74b61659e3a513f5342ff2da8c613bc93
崔庆才丨静觅 <1016903103@qq.com>
2019-07-29 17:14:08 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -8,24 +8,31 @@ import re
 
 
 def get_page(offset):
+    headers = {
+        'cookie': 'tt_webid=6667396596445660679; csrftoken=3a212e0c06e7821650315a4fecf47ac9; tt_webid=6667396596445660679; WEATHER_CITY=%E5%8C%97%E4%BA%AC; UM_distinctid=16b846003e03d7-0dd00a2eb5ea11-353166-1fa400-16b846003e1566; CNZZDATA1259612802=2077267981-1561291030-https%253A%252F%252Fwww.baidu.com%252F%7C1561361230; __tasessionId=4vm71cznd1561363013083; sso_uid_tt=47d6f9788277e4e071f3825a3c36a294; toutiao_sso_user=e02fd616c83dff880adda691cd201aaa; login_flag=6859a0b8ffdb01687b00fe96bbeeba6e; sessionid=21f852358a845d783bdbe1236c9b385b; uid_tt=d40499ec45187c2d411cb7bf656330730d8c15a783bb6284da0f73104cd300a2; sid_tt=21f852358a845d783bdbe1236c9b385b; sid_guard="21f852358a845d783bdbe1236c9b385b|1561363028|15552000|Sat\054 21-Dec-2019 07:57:08 GMT"; s_v_web_id=6f40e192e0bdeb62ff50fca2bcdf2944',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+        'referer': 'https://www.toutiao.com/search/?keyword=%E8%A1%97%E6%8B%8D',
+    }
     params = {
         'aid': '24',
+        'app_name': 'web_search',
         'offset': offset,
         'format': 'json',
-        #'keyword': '街拍',
+        'keyword': '街拍',
         'autoload': 'true',
         'count': '20',
+        'en_qc': '1',
         'cur_tab': '1',
         'from': 'search_tab',
-        'pd': 'synthesis'
+        'pd': 'synthesis',
     }
-    base_url = 'https://www.toutiao.com/api/search/content/?keyword=%E8%A1%97%E6%8B%8D'
+    base_url = 'https://www.toutiao.com/api/search/content/?'
     url = base_url + urlencode(params)
+    # print(url)
     try:
-        resp = requests.get(url)
-        print(url)
+        resp = requests.get(url, headers=headers)
         if 200  == resp.status_code:
-            print(resp.json())
             return resp.json()
     except requests.ConnectionError:
         return None
@@ -35,23 +42,20 @@ def get_images(json):
     if json.get('data'):
         data = json.get('data')
         for item in data:
-            if item.get('cell_type') is not None:
+            if item.get('title') is None:
                 continue
-            title = item.get('title')
+            title = re.sub('[\t]', '', item.get('title'))
             images = item.get('image_list')
             for image in images:
-                origin_image = re.sub("list", "origin", image.get('url'))
+                origin_image = re.sub("list.*?pgc-image", "large/pgc-image", image.get('url'))
                 yield {
-                    'image':  origin_image,
-                    # 'iamge': image.get('url'),
+                    'image': origin_image,
                     'title': title
                 }
 
-print('succ')
 
 def save_image(item):
     img_path = 'img' + os.path.sep + item.get('title')
-    print('succ2')
     if not os.path.exists(img_path):
         os.makedirs(img_path)
     try:
@@ -61,26 +65,23 @@ def save_image(item):
                 file_name=md5(resp.content).hexdigest(),
                 file_suffix='jpg')
             if not os.path.exists(file_path):
-                print('succ3')
                 with open(file_path, 'wb') as f:
                     f.write(resp.content)
                 print('Downloaded image path is %s' % file_path)
-                print('succ4')
             else:
                 print('Already Downloaded', file_path)
-    except requests.ConnectionError:
-        print('Failed to Save Image，item %s' % item)
+    except Exception as e:
+        print(e)
 
 
 def main(offset):
     json = get_page(offset)
     for item in get_images(json):
-        print(item)
         save_image(item)
 
 
 GROUP_START = 0
-GROUP_END = 7
+GROUP_END = 9
 
 if __name__ == '__main__':
     pool = Pool()
