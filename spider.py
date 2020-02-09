886bd7ead5fe11e70ffadf1b363dfc00c289e6a7
崔庆才丨静觅 <1016903103@qq.com>
2020-02-10 02:50:49 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -5,12 +5,12 @@ import os
 from hashlib import md5
 from multiprocessing.pool import Pool
 import re
-
+import random
 
 def get_page(offset):
     headers = {
-        'cookie': 'tt_webid=6667396596445660679; csrftoken=3a212e0c06e7821650315a4fecf47ac9; tt_webid=6667396596445660679; WEATHER_CITY=%E5%8C%97%E4%BA%AC; UM_distinctid=16b846003e03d7-0dd00a2eb5ea11-353166-1fa400-16b846003e1566; CNZZDATA1259612802=2077267981-1561291030-https%253A%252F%252Fwww.baidu.com%252F%7C1561361230; __tasessionId=4vm71cznd1561363013083; sso_uid_tt=47d6f9788277e4e071f3825a3c36a294; toutiao_sso_user=e02fd616c83dff880adda691cd201aaa; login_flag=6859a0b8ffdb01687b00fe96bbeeba6e; sessionid=21f852358a845d783bdbe1236c9b385b; uid_tt=d40499ec45187c2d411cb7bf656330730d8c15a783bb6284da0f73104cd300a2; sid_tt=21f852358a845d783bdbe1236c9b385b; sid_guard="21f852358a845d783bdbe1236c9b385b|1561363028|15552000|Sat\054 21-Dec-2019 07:57:08 GMT"; s_v_web_id=6f40e192e0bdeb62ff50fca2bcdf2944',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
+        'cookie': 'tt_webid=6787304267841324551; WEATHER_CITY=%E5%8C%97%E4%BA%AC; tt_webid=6787304267841324551; csrftoken=6c8d91e61b7db691bfa45021a0e7e511; UM_distinctid=16ffb7f1fcfec-0d6566ad15973e-396a4605-144000-16ffb7f1fd02eb; s_v_web_id=k631j38t_qMPq6VOD_jioN_4lgi_BQB1_GGhAVEKoAmXJ; __tasessionId=ocfmlmswt1580527945483',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
         'x-requested-with': 'XMLHttpRequest',
         'referer': 'https://www.toutiao.com/search/?keyword=%E8%A1%97%E6%8B%8D',
     }
@@ -36,56 +36,82 @@ def get_page(offset):
             return resp.json()
     except requests.ConnectionError:
         return None
-
-
+    
 def get_images(json):
+    headers = {
+        'cookie': 'tt_webid=6787304267841324551; WEATHER_CITY=%E5%8C%97%E4%BA%AC; tt_webid=6787304267841324551; csrftoken=6c8d91e61b7db691bfa45021a0e7e511; UM_distinctid=16ffb7f1fcfec-0d6566ad15973e-396a4605-144000-16ffb7f1fd02eb; s_v_web_id=k631j38t_qMPq6VOD_jioN_4lgi_BQB1_GGhAVEKoAmXJ; __tasessionId=ocfmlmswt1580527945483',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+        'referer': 'https://www.toutiao.com/search/?keyword=%E8%A1%97%E6%8B%8D',
+    }
     if json.get('data'):
         data = json.get('data')
         for item in data:
-            if item.get('title') is None:
+            if item.get('title') is None: # 刨掉前部分无关内容
                 continue
-            title = re.sub('[\t]', '', item.get('title'))
-            images = item.get('image_list')
-            for image in images:
-                origin_image = re.sub("list.*?pgc-image", "large/pgc-image", image.get('url'))
-                yield {
-                    'image': origin_image,
-                    'title': title
-                }
-
-
+            title = re.sub('[\t]', '', item.get('title')) # 获取标题
+            url = item.get("article_url")  #获取子链接
+            if url == None:
+                continue
+            try:
+                resp = requests.get(url,headers=headers)
+                if 200  == resp.status_code:
+                    images_pattern = re.compile('JSON.parse\("(.*?)"\),\n',re.S)
+                    result = re.search(images_pattern,resp.text)
+                    if result == None: # 非图集形式
+                        images = item.get('image_list')
+                        for image in images:
+                            origin_image = re.sub("list.*?pgc-image", "large/pgc-image", image.get('url')) # 改成origin/pgc-image是原图
+                            yield {
+                                'image': origin_image,
+                                'title': title
+                            }
+                    else: # 图集形式 抓取gallery下json格式数据
+                        url_pattern=re.compile('url(.*?)"width',re.S)
+                        result1 = re.findall(url_pattern,result.group(1))
+                        for i in range(len(result1)):
+                            yield{
+                                'image': "http://p%d.pstatp.com/origin/pgc-image/"%(random.randint(1,10)) + 
+                                           result1[i][result1[i].rindex("u002F")+5:result1[i].rindex('\\"')], #存储url
+                                'title': title
+                            }
+            except requests.ConnectionError: # 打开子链接失败就直接保存图集中前部分
+                for image in images:
+                    origin_image = re.sub("list.*?pgc-image", "large/pgc-image", image.get('url')) # 改成origin/pgc-image是原图
+                    yield {
+                        'image': origin_image,
+                        'title': title
+                    }
+                
 def save_image(item):
     img_path = 'img' + os.path.sep + item.get('title')
     if not os.path.exists(img_path):
-        os.makedirs(img_path)
+        os.makedirs(img_path) # 生成目录文件夹
     try:
         resp = requests.get(item.get('image'))
         if codes.ok == resp.status_code:
             file_path = img_path + os.path.sep + '{file_name}.{file_suffix}'.format(
-                file_name=md5(resp.content).hexdigest(),
-                file_suffix='jpg')
+                file_name=md5(resp.content).hexdigest(), 
+                file_suffix='jpg')  # 单一文件的路径
             if not os.path.exists(file_path):
                 with open(file_path, 'wb') as f:
-                    f.write(resp.content)
+                    f.write(resp.content) 
                 print('Downloaded image path is %s' % file_path)
             else:
                 print('Already Downloaded', file_path)
     except Exception as e:
         print(e)
-
-
+        
 def main(offset):
     json = get_page(offset)
     for item in get_images(json):
         save_image(item)
 
-
-GROUP_START = 0
-GROUP_END = 9
-
 if __name__ == '__main__':
+    '''
+    for i in range(3):
+        main(20*i)
+    '''
     pool = Pool()
-    groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
+    groups = ([x * 20 for x in range(0, 3)])
     pool.map(main, groups)
-    pool.close()
-    pool.join()
